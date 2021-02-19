7e5b153815881802872f4e6782f1ee28df8bda1d
emblema <emblema.project@gmail.com>
2021-02-18 18:40:19 -0600

    Resolve problem with outdated packages

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,13 @@
-const config = require('config');
+const config = require("config");
 
-module.exports = function() {
-  if (!config.get('jwtPrivateKey')) {
-    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+module.exports = function () {
+  console.log("+-+-+-+-+-+-+-+-+-+-+-+-+");
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.PORT);
+  console.log(config.get("jwtPrivateKey"));
+  console.log("+-+-+-+-+-+-+-+-+-+-+-+-+");
+
+  if (!config.get("jwtPrivateKey")) {
+    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
-}
+};
